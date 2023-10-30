6a9db84e10b877bb1b3622b28ea3dae6f7ca4e48
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2023-10-30 07:48:06 -0400

    added round-robin chooser for audio

---
+++ Scripts/Collections/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Extensions {
+	private static readonly Random _random = new Random();
+
+	public static void Shuffle<T>(this IList<T> items, Random random = null) {
+		if( random == null )
+			random = _random;
+		for( int i = items.Count - 1; i  > 0; --i ) {
+			int j = random.Next(0, i + 1);
+			(items[i], items[j]) = (items[j], items[i]);
+		}
+	}
+}
---
+++ Scripts/Collections/RoundRobin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections {
+	public class RoundRobin<T> {
+		private readonly List<T> _items;
+
+		private int _count;
+
+		public RoundRobin(int count = 0) {
+			_items = new List<T>(count);
+		}
+
+		public RoundRobin(IEnumerable<T> items) {
+			_items = new List<T>(items);
+		}
+
+		public T Get() {
+			if( _count >= _items.Count ) {
+				_items.Shuffle();
+				_count = 0;
+			}
+			return _items[_count++];
+		}
+	}
+}
