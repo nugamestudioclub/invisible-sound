8851f802a151f6f9c1c7502deb297dcaa7ad7a56
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2023-11-02 06:20:42 -0400

    added timer for player footsteps

--- Scripts/PlayerEntity.cs
+++ Scripts/PlayerEntity.cs
@@ -1,11 +1,19 @@
 using Collections;
 using Godot;
-using System;
 
 public class PlayerEntity : SceneServiceNode {
+	[Export]
+	private float _footstepSeconds = 0.5f;
+
+	private float _footstepElapsed = float.MinValue;
+
 	public PlayerEntity() {
 		Type = EntityType.Character;
 	}
+
+	private bool HasFootstepStarted => _footstepElapsed >= 0f;
+
+	private bool HasFootstepFinished => _footstepElapsed >= _footstepSeconds;
 
 	public void _Player_area_collision(KinematicBody2D player, Area2D area) {
 		// GD.Print($"{nameof(PlayerEntity)}.{nameof(_Player_area_collision)}");
@@ -13,6 +21,27 @@ public class PlayerEntity : SceneServiceNode {
 	}
 
 	public void _Player_footstep() {
+		if( !HasFootstepStarted )
+			StartFootstep();
+	}
+
+	public override void _PhysicsProcess(float delta) {
+		base._PhysicsProcess(delta);
+		if( HasFootstepStarted )
+			UpdateFootstep(delta);
+
+	}
+
+	private void StartFootstep() {
+		// TODO: Detect ground material
 		Entity.Services.AudioService.PlayFootstep("Dirt");
+		_footstepElapsed = 0f;
+	}
+
+	private void UpdateFootstep(float delta) {
+		if( HasFootstepFinished )
+			_footstepElapsed =  float.MinValue;
+		else if( HasFootstepStarted )
+			_footstepElapsed += delta;
 	}
 }
