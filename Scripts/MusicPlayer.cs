1833354ae8d789c772e8e5fa5a0aea270254bb3d
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2023-11-03 08:40:47 -0400

    started setting up music

---
+++ Scripts/MusicPlayer.cs
@@ -0,0 +1,10 @@
+﻿using Godot;
+
+public class MusicPlayer : SceneServiceNode {
+	[Export]
+	private Resource _resource;
+
+	public override void Start() {
+		Entity.Services.AudioService.PlayLooping(_resource.ResourcePath, (int)AudioTrack.Music, this);
+	}
+}
