fbbfe9fd0a99662aaa8c93ffc1a3b376b94f1623
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2023-11-03 18:07:35 -0400

    fixed crash on start

--- Scripts/Gate.cs
+++ Scripts/Gate.cs
@@ -2,17 +2,26 @@
 
 public class Gate : Node2D {
 	[Export]
-	private PlayerEntity _player;
-
-	[Export]
 	private float _radius = 2.5f;
 
+	private PlayerEntity _player;
+
+	private Game _game;
+
 	public override void _PhysicsProcess(float delta) {
-		bool hasKey =_player.Entity.Game.GlobalData.GetValueOrDefault("hasKey", false);
-		float distance = GlobalPosition.DistanceTo(_player.GlobalPosition);
-		if( hasKey && distance <=_radius ) {
-			var scene = GD.Load<PackedScene>("res://Scenes/Menus/Credits.tscn");
-			GetTree().ChangeSceneTo(scene);
+		if( _player == null ) {
+			var entities = GetTree().GetNodesInGroup("Player");
+			foreach( var entity in entities )
+				if( entity is PlayerEntity playerEntity )
+					_player = playerEntity;
+		}
+		else {
+			bool hasKey =_player.Entity.Game.GlobalData.GetValueOrDefault("hasKey", false);
+			float distance = GlobalPosition.DistanceTo(_player.GlobalPosition);
+			if( hasKey && distance <=_radius ) {
+				var scene = GD.Load<PackedScene>("res://Scenes/Menus/Credits.tscn");
+				GetTree().ChangeSceneTo(scene);
+			}
 		}
 	}
 }
