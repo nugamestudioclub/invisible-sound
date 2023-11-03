11e97c894b88e9d00117c6165f06e3b2181eaae8
sudzoof <60050712+sudzoof@users.noreply.github.com>
2023-11-03 01:48:45 -0400

    Create InventoryUI.tscn

---
+++ Scripts/InventoryUI.gd
@@ -0,0 +1,13 @@
+tool
+extends Control
+
+onready var gas_container = $HBoxContainer/GasContainer
+onready var card_container = $HBoxContainer/CardContainer
+
+export var has_gas : bool = false
+export var has_card : bool = false
+
+func _process(delta):
+	if Engine.is_editor_hint():
+		gas_container.visible = has_gas
+		card_container.visible = has_card
